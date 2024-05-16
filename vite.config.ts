64d55bc82cc52cde91310175be78fc134679f210
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-05-16 23:15:54 +0300

    init: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+    }
+  }
+})
