b19798bf28eaeeefce372ff1d9c13e35d5bf2492
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-05-17 04:37:16 +0300

    fix: fix some issues

--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -7,32 +7,32 @@ export function Footer() {
                 <span className={cls.copyright}>© 2018 «LoremIpsum.net» Все права<br />защищены.</span>
                 <span className={cls.divider} />
                 <div className={cls.walletsWrapper}>
-                    <span className={cls.contactWrapper}>
+                    <a className={cls.contactWrapper}>
                         <img src="/images/qiwi.svg" alt="qiwi" />
                         <span>QIWI wallet</span>
-                    </span>
+                    </a>
 
-                    <span className={cls.contactWrapper}>
+                    <a className={cls.contactWrapper}>
                         <img src="/images/yandex-money.svg" alt="Yandex Money" />
                         <span>Yandex Money</span>
-                    </span>
+                    </a>
 
-                    <span className={cls.contactWrapper}>
+                    <a className={cls.contactWrapper}>
                         <img src="/images/web-money.svg" alt="Web Money" />
                         <span>Web Money</span>
-                    </span>
+                    </a>
                 </div>
                 <span className={cls.divider} />
                 <div className={cls.contactsWrapper}>
-                    <span className={cls.contactWrapper}>
+                    <a className={cls.contactWrapper}>
                         <img src="/images/email.svg" alt="email" />
                         <span>info@ipsum228.com</span>
-                    </span>
+                    </a>
 
-                    <span className={cls.contactWrapper}>
+                    <a className={cls.contactWrapper}>
                         <img src="/images/email.svg" alt="email" />
                         <span>Мы вконтакте</span>
-                    </span>
+                    </a>
                 </div>
             </div>
         </footer>
