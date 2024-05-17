eb9c888a5a088f65bcbf38ee33cec99b9177a24a
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-05-17 12:28:22 +0300

    feat: change dropdown menu

--- src/components/select-input/select-input.tsx
+++ src/components/select-input/select-input.tsx
@@ -2,12 +2,14 @@ import cls from './select-input.module.scss'
 
 export function SelectInput() {
     return (
-        <select className={cls.select}>
-            <option value="Sed ut perspiciatis">Sed ut perspiciatis</option>
-            <option value="Nemo enim ipsam">Nemo enim ipsam</option>
-            <option value="Et harum quidem">Et harum quidem</option>
-            <option value="Temporibus autem">Temporibus autem</option>
-            <option value="Itaque earum rerum">Itaque earum rerum</option>
-        </select>
+        <div className={cls.selecteWrapper}>
+            <select className={cls.select}>
+                <option value="Sed ut perspiciatis">Sed ut perspiciatis</option>
+                <option value="Nemo enim ipsam">Nemo enim ipsam</option>
+                <option value="Et harum quidem">Et harum quidem</option>
+                <option value="Temporibus autem">Temporibus autem</option>
+                <option value="Itaque earum rerum">Itaque earum rerum</option>
+            </select>
+        </div>
     )
 }
